b63ea8d0641c8ba69f038c678886ab6593143318
xKarmavii <giusepperomano7900@gmail.com>
2024-10-22 15:22:14 +0200

    prova commit files

---
+++ src/main/java/com/example/progetto_escursioni/ProgettoEscursioniApplication.java
@@ -0,0 +1,13 @@
+package com.example.progetto_escursioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoEscursioniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoEscursioniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/progetto_escursioni/ProgettoEscursioniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progetto_escursioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoEscursioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
