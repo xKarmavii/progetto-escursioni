08e4491ee46cc4030c9174090bb724bca32b8e7d
Giacomo-Mazzoni <giacomo-mazzoni@outlook.it>
2024-10-23 10:45:56 +0200

    Creato il model Utente

---
+++ src/main/java/com/example/progetto_escursioni/model/Utente.java
@@ -0,0 +1,91 @@
+package com.example.progetto_escursioni.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "utenti")
+public class Utente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String nome;
+    @Column
+    private String cognome;
+    @Column(name = "data_nascita")
+    private LocalDate dataNascita;
+    @Column
+    private String email;
+    @Column
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private String carta;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public LocalDate getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(LocalDate dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCarta() {
+        return carta;
+    }
+
+    public void setCarta(String carta) {
+        this.carta = carta;
+    }
+}
