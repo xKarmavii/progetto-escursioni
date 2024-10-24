824c0b449cebbb9ac7419214a2051025968efa2b
Nicola Tango <nicola.catello01@gmail.com>
2024-10-24 14:59:34 +0200

    Risolto problema messaggio d'errore in caso di credenziali errate e messaggio di successo in caso di registrazione effettuata

--- src/main/java/com/example/progetto_escursioni/controller/LoginRegistrazioneController.java
+++ src/main/java/com/example/progetto_escursioni/controller/LoginRegistrazioneController.java
@@ -1,14 +1,12 @@
 package com.example.progetto_escursioni.controller;
 
+import com.example.progetto_escursioni.model.Utente;
 import com.example.progetto_escursioni.service.UtenteService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/loginregistrazione")
@@ -18,13 +16,16 @@ public class LoginRegistrazioneController {
     private UtenteService utenteService;
 
     @GetMapping
-    public String getPage() {
+    public String getPage(Model model) {
+        Utente utente = new Utente();
+        model.addAttribute("utente", utente);
         return "login-registrazione";
     }
 
     @PostMapping("/login")
     public String loginManager(HttpSession session,
                                Model model,
+                               @ModelAttribute("utente") Utente utente,
                                @RequestParam("login-username") String username,
                                @RequestParam("login-password") String password) {
 
@@ -32,13 +33,17 @@ public class LoginRegistrazioneController {
             return "redirect:/areariservata";
         }
         else {
-            model.addAttribute("erroreLogin", "Credenziali errate");
+            model.addAttribute("messaggio", "Credenziali errate");
         }
         return "login-registrazione";
     }
 
     @PostMapping("/registrazione")
-    public String registrazioneManager() {
-        return null;
+    public String registrazioneManager(Model model,
+                                       @ModelAttribute("utente") Utente utente) {
+
+        utenteService.salvaUtente(utente);
+        model.addAttribute("messaggio", "Registrazione effettuata");
+        return "login-registrazione";
     }
 }
